50c4417fb4c010f7440c3963d987e5a89a62d825
wetcircle <65508703+wetcircle@users.noreply.github.com>
2021-12-05 10:31:41 -0500

    deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+    // basic deploy script
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, Hardhat!");
+  
+    console.log("Greeter deployed to:", greeter.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  
