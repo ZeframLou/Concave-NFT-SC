4bc55a6188625dda75aa4a0019fb0d74dec0c4bc
jbtobar <jbtobar.io@gmail.com>
2021-12-06 06:56:32 -0500

    fork mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -22,8 +23,14 @@ module.exports = {
     url: "https://eth-rinkeby.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
     accounts: [process.env.PRIVATE_KEY]
   },*/
+  // hardhat: {
+  //   chainId: 1337
+  // },
   hardhat: {
-    chainId: 1337
+    forking: {
+      url: process.env.API_MAINNET,
+      blockNumber: 13752124
+    }
   }
 },
   solidity: "0.8.4",
