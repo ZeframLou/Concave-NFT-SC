86497c94f76c70cd7400a90f1f741b2ad7a16deb
jbtobar <jbtobar.io@gmail.com>
2021-12-06 06:50:57 -0500

    test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,31 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("ConcaveNFT", function () {
+  let ConcaveNFT
+  const _name = "name"
+  const _symbol = "symbol"
+  const _initBaseURI = "_initBaseURI"
+  const _initNotRevealedUri = "_initNotRevealedUri"
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  it("ConcaveNFT should deploy", async function () {
+    ConcaveNFT = await ethers.getContractFactory("ConcaveNFT");
+    const concavenft = await ConcaveNFT.deploy(
+      _name,
+      _symbol,
+      _initBaseURI,
+      _initNotRevealedUri
+    );
+    console.log('concavenft deployed')
+    // await greeter.deployed();
+    //
+    // expect(await greeter.greet()).to.equal("Hello, world!");
+    //
+    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    //
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait();
+    //
+    // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
