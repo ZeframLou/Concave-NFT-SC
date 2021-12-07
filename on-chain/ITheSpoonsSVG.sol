d727a00865306a805d450f7bd25fd37ba84fdcb6
jbtobar <jbtobar.io@gmail.com>
2021-12-06 22:04:45 -0500

    wip on-chain

---
+++ on-chain/ITheSpoonsSVG.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+interface ITheSpoonsSVG {
+  function generateSVGImage(uint256 tokenId, string memory hexCode, string memory role, string memory opposite,uint32 r,uint32 g, uint32 b, uint8 material) external view returns (string memory);
+}
